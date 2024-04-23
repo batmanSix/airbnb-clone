e2f837a22e3f43e66c753530ccf80fa5e97947a4
神毓逍遥 <841331654@qq.com>
2024-04-23 11:14:32 +0800

    feat:nav组件

---
+++ src/app/components/Navbar.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import Image from "next/image";
+import Link from "next/link";
+import DesktopLogo from "../../../public/airbnb-desktop.png";
+import MobileLogo from "../../../public/airbnb-mobile.webp";
+function Navbar() {
+  return (
+    <nav className="w-full border-b">
+      <div className="flex items-center justify-between container mx-auto px-5 lg:px-10 py-5">
+        <Link href="/">
+          <Image
+            src={DesktopLogo}
+            alt="Desktop Logo"
+            className="w-32 hidden lg:block"
+          />
+
+          <Image
+            src={MobileLogo}
+            alt="Mobile Logo"
+            className="block lg:hidden w-12"
+          />
+        </Link>
+      </div>
+    </nav>
+  );
+}
+
+export default Navbar;
