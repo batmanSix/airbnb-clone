140951f0e1c36de6c29feb24bb22b9ff10121b05
神毓逍遥 <841331654@qq.com>
2024-04-24 18:42:51 +0800

    search loging 组件开发

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,9 @@ import Image from "next/image";
 import { MapFilterItems } from "./components/MapFilterItems";
 import prisma from "@/lib/db";
 import ListingCard from "./components/ListingCard";
-
+import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
+import { Suspense } from "react";
+import { SkeltonCard } from "./components/SkeltonCard";
 async function getData({
   searchParams
 }: {
@@ -29,6 +31,39 @@ async function getData({
   return data;
 }
 
+
+async function ShowItems({
+  searchParams,
+}: {
+  searchParams?: {
+    filter?: string;
+    country?: string;
+    guest?: string;
+    room?: string;
+    bathroom?: string;
+  };
+}) {
+
+  const wait = (seconds: number) => new Promise((resolve) => setTimeout(resolve, seconds * 1000));
+  await wait(1.5); // 
+  const data = await getData({ searchParams: searchParams });
+  return (
+    <div className="grid lg:grid-cols-4 sm:grid-clos-2 md:grid-clos=3 gap-8 mt-8">
+      {data.map((item, index) => {
+        return (
+          <ListingCard
+            key={item.id}
+            description={item.description as string}
+            imagePath={item.photo as string}
+            location={item.country as string}
+            price={item.price as number}
+          ></ListingCard>
+        );
+      })}
+    </div>
+  )
+}
+
 export default async function Home({
   searchParams
 }: {
@@ -36,23 +71,30 @@ export default async function Home({
     filter?: string
   }
 }) {
-  const data = await getData({ searchParams: searchParams });
+
   return (
     <div className="container mx-auto px-5 lg:px-10">
       <MapFilterItems></MapFilterItems>
-      <div className="grid lg:grid-cols-4 sm:grid-clos-2 md:grid-clos=3 gap-8 mt-8">
-        {data.map((item, index) => {
-          return (
-            <ListingCard
-              key={item.id}
-              description={item.description as string}
-              imagePath={item.photo as string}
-              location={item.country as string}
-              price={item.price as number}
-            ></ListingCard>
-          );
-        })}
-      </div>
+      <Suspense key={searchParams?.filter} fallback={<SkeletonLoading></SkeletonLoading>}>
+        <ShowItems searchParams={searchParams}></ShowItems>
+      </Suspense>
+    </div>
+  );
+}
+
+
+function SkeletonLoading() {
+  return (
+    <div className="grid lg:grid-cols-4 sm:grid-cols-2 md:grid-cols-3 gap-8 mt-8">
+      <SkeltonCard />
+      <SkeltonCard />
+      <SkeltonCard />
+      <SkeltonCard />
+      <SkeltonCard />
+      <SkeltonCard />
+      <SkeltonCard />
+      <SkeltonCard />
+      <SkeltonCard />
     </div>
   );
 }
