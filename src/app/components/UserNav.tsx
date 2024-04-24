df132baf913e805ceb1ed9797dcf6a6cf00de1b1
神毓逍遥 <841331654@qq.com>
2024-04-24 10:17:16 +0800

    feat:链接本地mysql

--- src/app/components/UserNav.tsx
+++ src/app/components/UserNav.tsx
@@ -40,7 +40,30 @@ async function UserNav() {
         {user ? (
           <>
             <DropdownMenuItem>
-              <LogoutLink>退出</LogoutLink>
+              <form className="w-full">
+                <button type="submit" className="w-full text-start">
+                  Airbnb your Home
+                </button>
+              </form>
+            </DropdownMenuItem>
+            <DropdownMenuItem>
+              <Link href="/my-homes" className="w-full">
+                My Listings
+              </Link>
+            </DropdownMenuItem>
+            <DropdownMenuItem>
+              <Link href="/favorites" className="w-full">
+                My Favorites
+              </Link>
+            </DropdownMenuItem>
+            <DropdownMenuItem>
+              <Link href="/reservations" className="w-full">
+                My Reservations
+              </Link>
+            </DropdownMenuItem>
+            <DropdownMenuSeparator />
+            <DropdownMenuItem>
+              <LogoutLink className="w-full">Logout</LogoutLink>
             </DropdownMenuItem>
           </>
         ) : (
